3573bad566a3860351b0749f77d896f8da4c26a6
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-12-31 11:38:20 +0300

    Add resize_avatar function to serverless
     - Add uppercase versions of .png, .jpg, .jpeg, .gif suffixes to serverless s3 events
     - Update serverless s3 event prefix of resize_default function

---
+++ src/avatar/handler.js
@@ -0,0 +1,18 @@
+import { getFileInformation } from '../s3'
+import { AVATAR_SIZE_LIST } from '../constants'
+import { resize } from '../resize'
+import { getDestinationBucket } from '../env'
+
+export const main = async (event, context, callback) => {
+  const { eventName, bucket, key } = getFileInformation(event)
+
+  console.log(`Received ${eventName} for item in bucket: ${bucket}, key: ${key}`)
+
+  try {
+    const buckets = { sourceBucket: bucket, destinationBucket: getDestinationBucket() }
+    await resize(buckets, key, AVATAR_SIZE_LIST)
+  } catch (error) {
+    callback(error)
+  }
+
+}
--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,13 @@
-const THUMB = {
-  dimension: '113x113',
-  text: 'thumb',
-}
+const THUMB = { dimension: '113x113', text: 'thumb' }
 
-const MEDIUM = {
-  dimension: '234x234',
-  text: 'medium',
-}
+const MEDIUM = { dimension: '234x234', text: 'medium' }
 
-const LARGE = {
-  dimension: '480x480',
-  text: 'large',
-}
+const LARGE = { dimension: '480x480', text: 'large' }
+
+export const AVATAR_SIZE_LIST = [
+  { dimension: '36x36', text: 'thumb', },
+  { dimension: '160x160', text: 'medium' }
+]
 
 export const DEFAULT_SIZE_LIST = [THUMB, MEDIUM, LARGE];
 
